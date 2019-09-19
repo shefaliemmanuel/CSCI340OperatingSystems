5ca5aee3a4a3256649e5a4fa415daa0dfeb31245
shefaliemmanuel <emmanuelsn@g.cofc.edu>
2019-09-19 11:57:27 -0400

    string comparison complete

---
+++ Lab4/StringComparison.c
@@ -0,0 +1,51 @@
+//
+//  StringComparison.c
+//  
+//
+//  Created by Shefali Emmanuel on 9/19/19.
+//
+
+#include <string.h>
+
+#include <stdio.h>
+
+int main()
+
+{
+    
+    // Try different combinations of string pairs
+    
+    // apple vs apple
+    //    if the number is the same then it should return 0
+    
+    // apple vs apples
+//    -115
+    
+    // apple vs zebra
+//    -25
+    
+    // zebra vs apple
+//    25
+    
+    // apple vs APPLE
+//    32
+    
+    // APPLE vs apple
+//    -32
+    
+    char* S1 = strdup("apple");
+    
+    char* S2 = strdup("apple");
+    
+// it is an addition of  of ascii characters
+//compares one charater at a time
+// Positive Result: If left char is lexicographically after the right char
+// Negative Result: If left char is lexicographically before the right char
+    
+//I came to this conclusion through testing and reference to https://www.geeksforgeeks.org/strcmp-in-c-cpp/
+    
+    int result = strcmp(S1, S2);
+    
+    printf("%d\n", result);
+    
+}
