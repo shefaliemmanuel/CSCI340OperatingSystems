5eccdc77f3da7bf62fe97d62d84e85870edb6a27
shefaliemmanuel <emmanuelsn@g.cofc.edu>
2019-09-19 12:52:24 -0400

    homework

---
+++ 340/340/hw1.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  340
+//
+//  Created by Shefali Emmanuel on 9/19/19.
+//  Copyright © 2019 Shefali Emmanuel. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
