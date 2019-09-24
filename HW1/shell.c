f38a35f8146cfc1d91c25cfaa493d12b5ae714e0
Shefali Emmanuel <emmanuelsn@g.cofc.edu>
2019-09-24 14:11:20 -0400

    Update shell.c

--- HW1/shell.c
+++ HW1/shell.c
@@ -3,7 +3,7 @@
 //  340
 //
 //  Created by Shefali Emmanuel on 9/19/19.
-//  Copyright © 2019 Shefali Emmanuel. All rights reserved.
+//  Copyright B) 2019 Shefali Emmanuel. All rights reserved.
 //
 
 #include <stdio.h>
@@ -14,70 +14,79 @@
 
 #define argumentLength 100
 
-int main() {
+int
+main ()
+{
 
-    printf("hello world");
-    int spaceUsed = 0;
-    char *argumentBuffer[argumentLength];
+  printf ("hello world");
+  int spaceUsed = 0;
+  char *argumentBuffer[argumentLength];
 
-    printf("Please enter a command: ")
+  printf ("Please enter a command: ")
+    while (fgets (argumentBuffer, argumentLength, stdin)
+	   && strcmp (argumentBuffer, "exit\n") != 0)
+    {
+      //memset(argumentBuffer, 0, argumentLength);
 
-    while(fgets(argumentBuffer, argumentLength, stdin) && strcmp(argumentBuffer,"exit\n")!=0){
-        //memset(argumentBuffer, 0, argumentLength);
+      char *pch;
+      printf ("Splitting string \"%s\" into tokens:\n", argumentBuffer);
+      pch = strtok (argumentBuffer, " ");	// used space to generate new tokens
+      argumentBuffer[spaceUsed] = pch;
+      spaceUsed++;
 
-char * pch;
-            printf ("Splitting string \"%s\" into tokens:\n",argumentBuffer);
-        pch = strtok (argumentBuffer," "); // used space to generate new tokens
-        argumentBuffer[spaceUsed] = pch;
-        spaceUsed++;
+      while (pch != NULL)
+	{
+	  printf ("%s\n", pch);	//allocates mem and returns a pointer to the memory
+	  pch = strtok (NULL, " ");
+	  argumentBuffer[spaceUsed] = *pch;
+	  // get rid of & symbol if its the 2nd to last
+	}			//end while pch 
 
-        while (pch != NULL){
-            printf ("%s\n",pch); //allocates mem and returns a pointer to the memory
-            pch = strtok (NULL, " ");
-            argumentBuffer[spaceUsed] = *pch;
-            // get rid of & symbol if its the 2nd to last
-}//end while pch 
+      bool isBackgroupJob = false;
+      if (strcmp (argumentBuffer[spaceUsed - 2], "&") == 0)
+	{
+	  argumentBuffer[spaceUsed - 2] = NULL;
+	  isBackgrounpJob = true;
+	  //BOLEAN that says this is a backgroup job flag variable
+	}
 
-            bool isBackgroupJob =false;
-            if (strcmp(argumentBuffer[spaceUsed-2], "&") == 0){
-                argumentBuffer[spaceUsed-2] = NULL;
-                isBackgrounpJob=true;
-                //BOLEAN that says this is a backgroup job flag variable
-            }
+      int rc = fork ();
 
-        int rc = fork();
-    
-  
-        // fork failed; exit
-        if (rc < 0) {
-            fprintf(stderr, "fork failed\n");
-            exit(1);
-        }
-        // child (new process)
-        else if (rc == 0) {
-            printf("hello, I am child (pid:%d)\n", (int) getpid());
 
-            
-            execvp(argumentBuffer[0], argumentBuffer);
-        }
+      // fork failed; exit
+      if (rc < 0)
+	{
+	  fprintf (stderr, "fork failed\n");
+	  exit (1);
+	}
+      // child (new process)
+      else if (rc == 0)
+	{
+	  printf ("hello, I am child (pid:%d)\n", (int) getpid ());
 
-    // parent goes down this path (original process)
-    else {
- 
-        //take out the & symbol
-        if(isBackgrounpJob == false){
 
-int wc = wait(NULL);
+	  execvp (argumentBuffer[0], argumentBuffer);
+	}
 
-}
-        printf("hello, I am parent of %d (pid:%d)\n",
-                rc, (int) getpid());
+      // parent goes down this path (original process)
+      else
+	{
+
+	  //take out the & symbol
+	  if (isBackgrounpJob == false)
+	    {
+
+	      int wc = wait (NULL);
+
+	    }
+	  printf ("hello, I am parent of %d (pid:%d)\n", rc, (int) getpid ());
+	}
+
+
+      printf ("Goodbye.\n");
+
     }
+
+  return 0;
 }
 }
-
-printf("Goodbye.\n");
-
-return 0;
-}
-
