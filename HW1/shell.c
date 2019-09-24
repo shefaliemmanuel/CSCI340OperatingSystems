4569d10609caaa25421a5b032ab3ea4e738f1fe8
Shefali Emmanuel <emmanuelsn@g.cofc.edu>
2019-09-24 14:39:37 -0400

    Update shell.c

--- HW1/shell.c
+++ HW1/shell.c
@@ -11,82 +11,55 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define argumentLength 100
 
-int
-main ()
-{
+int main (){
+    printf ("hello world");
+    int spaceUsed = 0;
+    bool backgroundJobStatus = false;
+    char *argumentBuffer[argumentLength];
+    int wc;
 
-  printf ("hello world");
-  int spaceUsed = 0;
-  char *argumentBuffer[argumentLength];
-
-  printf ("Please enter a command: ")
-    while (fgets (argumentBuffer, argumentLength, stdin)
-	   && strcmp (argumentBuffer, "exit\n") != 0)
-    {
-      //memset(argumentBuffer, 0, argumentLength);
-
-      char *pch;
-      printf ("Splitting string \"%s\" into tokens:\n", argumentBuffer);
-      pch = strtok (argumentBuffer, " ");	// used space to generate new tokens
-      argumentBuffer[spaceUsed] = pch;
-      spaceUsed++;
-
-      while (pch != NULL)
-	{
-	  printf ("%s\n", pch);	//allocates mem and returns a pointer to the memory
-	  pch = strtok (NULL, " ");
-	  argumentBuffer[spaceUsed] = *pch;
-	  // get rid of & symbol if its the 2nd to last
-	}			//end while pch 
-
-      bool isBackgroupJob = false;
-      if (strcmp (argumentBuffer[spaceUsed - 2], "&") == 0)
-	{
-	  argumentBuffer[spaceUsed - 2] = NULL;
-	  isBackgrounpJob = true;
-	  //BOLEAN that says this is a backgroup job flag variable
-	}
-
-      int rc = fork ();
-
-
-      // fork failed; exit
-      if (rc < 0)
-	{
-	  fprintf (stderr, "fork failed\n");
-	  exit (1);
-	}
-      // child (new process)
-      else if (rc == 0)
-	{
-	  printf ("hello, I am child (pid:%d)\n", (int) getpid ());
-
-
-	  execvp (argumentBuffer[0], argumentBuffer);
-	}
-
+    printf ("Please enter a command: ");
+    while (fgets (argumentBuffer, argumentLength, stdin) && strcmp (argumentBuffer, "exit\n") != 0){
+        //memset(argumentBuffer, 0, argumentLength);
+        char *pch;
+        printf ("Splitting string \"%s\" into tokens:\n", argumentBuffer);
+        pch = strtok (argumentBuffer, " ");	// used space to generate new tokens
+        argumentBuffer[spaceUsed] = pch;
+        spaceUsed++;
+        while (pch != NULL){
+            printf ("%s\n", pch);	//allocates mem and returns a pointer to the memory
+            pch = strtok (NULL, " ");
+            argumentBuffer[spaceUsed] = pch;
+        }//end while pch
+        if (strcmp (argumentBuffer[spaceUsed - 2], "&") == 0){
+            argumentBuffer[spaceUsed - 2] = NULL;
+            backgroundJobStatus = true;
+        }
+        int rc = fork ();
+        // fork failed; exit
+        if (rc < 0){
+            fprintf (stderr, "fork failed\n");
+            exit (1);
+        }
+        // child (new process)
+        else if (rc == 0){
+            printf ("hello, I am child (pid:%d)\n", (int) getpid ());
+            execvp (argumentBuffer[0], argumentBuffer);
+        }
+        
       // parent goes down this path (original process)
-      else
-	{
-
+      else{
 	  //take out the & symbol
-	  if (isBackgrounpJob == false)
-	    {
-
-	      int wc = wait (NULL);
-
-	    }
-	  printf ("hello, I am parent of %d (pid:%d)\n", rc, (int) getpid ());
-	}
-
-
-      printf ("Goodbye.\n");
-
+          if (backgroundJobStatus == false){
+              wc = wait (NULL);
+          }
+          printf ("hello, I am parent of %d (pid:%d)\n", rc, (int) getpid ());
+      }
+    printf ("Goodbye.\n");
     }
-
   return 0;
-}
 }
