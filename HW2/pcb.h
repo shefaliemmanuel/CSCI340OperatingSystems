bef22bfa1fb4e18df125a96e63e7631ad4b258dd
Shefali Emmanuel <emmanuelsn@g.cofc.edu>
2019-10-08 10:32:39 -0400

    Update pcb.h

--- HW2/pcb.h
+++ HW2/pcb.h
@@ -7,8 +7,16 @@ typedef struct{
 // comments from pcb.c
 
 void init_process(PCB * process, int pid, int totalTime, int remainingTime);
+// process - Pointer to a PCB
+// post-condition: assigns all attributes to have value 0.
 
 void runProcess(PCB * process, int time);
+// process - Pointer to a PCB
+// runTime - Number of seconds to run this process on CPU
+// post-condition: Decrease process remainingTime by 
+// runTime only if runTime is >= remainingTime; else,
+// decrease process remainingTime by remainingTime (zero it out),
+// if remainingTime < runTime
 
 void printProcesses(PCB p);
 
