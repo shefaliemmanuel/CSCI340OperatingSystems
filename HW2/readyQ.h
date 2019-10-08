b41e82005120733de3855888af384d22f8a0b278
Shefali Emmanuel <emmanuelsn@g.cofc.edu>
2019-10-08 10:34:43 -0400

    Update readyQ.h

--- HW2/readyQ.h
+++ HW2/readyQ.h
@@ -10,13 +10,36 @@ typedef struct{
 }ReadyQ;
 
 void empty(ReadyQ *Q);
+// Q - pointer to a ReadyQueue
+// post-conditon: sets numProcesses = 0
 
 bool isEmpty(ReadyQ *Q);
+// Q - pointer to a ReadyQueue
+// returns true if queue is empty - contains no processes; else, false.
+
+bool isFull(ReadyQ *Q);
+// Q - pointer to a ReadyQueue
+// returns true if queue is full - contains MAX_Q_LEN processes; else, false.
 
 int sizeOfQ(ReadyQ *Q);
+// Q - pointer to a ReadyQueue
+// returns number of processes stored in the queue.
 
 bool insert(ReadyQ *Q, PCB *process);
+// Q - pointer to a ReadyQueue
+// process - pointer to a PCB
+// post-condition: if Q is not full, then insert process
+// into next open processList array slot, increment
+// numProcesses, and return true; else, return false.
 
 PCB*remove(ReadyQ *Q);
 
 void bubbleSort(ReadyQ *Q);
+// Q - pointer to a ReadyQueue
+// post-condition: Sorts contents of queue into
+// increasing order of remainingTime.  PCB with lowest
+// remainingTime will occupy list slot index 0.
+
+void print(ReadyQueue* Q);
+// Q - pointer to a ReadyQueue
+// post-condition: prints out information for each PCB in the queue.
